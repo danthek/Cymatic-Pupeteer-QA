376c89cf21e87402f4c3ee3a56d8a37ebb07b8a6
danthek <abraham8507@gmail.com>
2022-07-25 20:21:09 -0500

    Refactor register component

--- scenarios/client-site/register.js
+++ scenarios/client-site/register.js
@@ -1,47 +1,39 @@
 //Registration creds random automated generation
 
-function makeid(length) {
+function generate(field, length) {
   var result = '';
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  switch (field) {
+    case 'id':
+      var characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      break;
+    case 'mail':
+      var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      break;
+    case 'pass':
+      var characters =
+        'AABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?[]^_{|}~';
+  }
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  return result;
-}
-
-function makeMail(length) {
-  var result = '';
-  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-function makePass(length) {
-  var result = '';
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"$%&()*+,-./:;<=>?[]^_{|}~';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+  length = '';
   return result;
 }
 
 //asign strings to variables
-const newId = makeid(6);
-const newMail = makeMail(8);
+let field;
+let length;
+const newId = generate((field = 'id'), (length = 6));
+const newMail = generate((field = 'mail'), (length = 6));
 const newUser = `${newId}@${newMail}.com`;
-let newPass = makePass(8);
+let newPass = generate((field = 'pass'), (length = 10));
 
 //validate password criterias
 function checkRegex(newPass) {
   validPass = false;
-  var checkSpecial = /[*@!%&()^~{}]+/.test(newPass),
+  var checkSpecial = /[*@!#%&()^~{}]+/.test(newPass),
     checkUpper = /[A-Z]+/.test(newPass),
     checkLower = /[a-z]+/.test(newPass);
   checkMin = /^(?=.*\d).{8,}$/.test(newPass);
@@ -55,7 +47,7 @@ function checkRegex(newPass) {
 
 checkRegex(newPass);
 while (!validPass) {
-  newPass = makePass(8);
+  newPass = generate((field = 'pass'), (length = 10));
   checkRegex(newPass);
 }
 //main component
@@ -68,6 +60,7 @@ module.exports = async function (browser) {
 
   await page.type('#form > input:nth-child(2)', newUser, { delay: 100 });
   await page.type('#pass', newPass, { delay: 100 });
+  await page.waitForTimeout(6000);
   await page.type('#form > input:nth-child(6)', newPass, { delay: 100 });
   await page.click('#root > div > div > div > div.card > div > form > button');
   await page.waitForNavigation();
@@ -75,7 +68,10 @@ module.exports = async function (browser) {
   await page.click(
     ' #root > div > div > div > div.d-flex.align-items-center.container > div > button'
   );
-
+  console.log(newId);
+  console.log(newMail);
+  console.log(newUser);
+  console.log(newPass);
   await page.close();
 
   return { Register: 'worked' };
